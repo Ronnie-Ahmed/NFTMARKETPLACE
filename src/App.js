1485e61bbc3132229db69b8d427bffc5d928b663
Ronnie-Ahmed <rksraisul@gmail.com>
2023-09-08 17:35:19 +0600

    newFiles

--- src/App.js
+++ src/App.js
@@ -2,8 +2,37 @@ import "./App.css";
 import { Header } from "./Components/Header";
 import { Footer } from "./Components/Footer";
 import { Allroutes } from "./Components/Allroutes";
+import { useEffect } from "react";
+import { useConnectionStatus } from "@thirdweb-dev/react";
 
 function App() {
+  const status = useConnectionStatus();
+
+  const [pageChainId, setPageChainId] = useState(0);
+
+  const changeChainID = async () => {
+    if (status === "connected") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const chainId = await signer.getChainId();
+      setPageChainId(chainId);
+      if (chainId !== 0x13881) {
+        await window.ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [
+            {
+              chainId: "0x13881",
+            },
+          ],
+        });
+      }
+    }
+  };
+
+  useEffect(() => {
+    changeChainID();
+  }, [pageChainId]);
+
   return (
     <div>
       <Header />
